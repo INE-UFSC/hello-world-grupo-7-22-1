a2788f92628d7516b45ef7a13723766fa68291c9
Tiago Luz <email@example.comtiagoluz64@gmail.com>
2022-04-23 15:17:48 -0300

    added membro4 to app. added membro1 info

--- membro1.py
+++ membro1.py
@@ -1,2 +1,4 @@
 def hello():
-	return "Olá, eu sou o membro1" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+
+	return "Olá, eu sou Tiago Oliveira da Luzz\nIdade: 19\nHobbies: Jogar(RPG, FPS, Estratégia), Ler, Música, Basquete, Vôlei\nComecei programando em C e para Arduino, tenho algum conhecimento em C++ e mais recentemente focado em Python\nEspero nessa disciplina aprender em mais detalhe os conceitos envolvido em POO e métodos para organizar problemas usando POO" 
+    #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
