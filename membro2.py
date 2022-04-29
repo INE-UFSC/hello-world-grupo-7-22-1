5e049e8bc0ee4b505a8193db3d1f553dd2a8c3e5
Leonardo-Hahn <74681223+Leonardo-Hahn@users.noreply.github.com>
2022-04-29 02:38:17 -0300

    Update membro2.py

--- membro2.py
+++ membro2.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro2" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou o Leonardo Hahn, tenho 20 anos e meus hobbies eram ler, jogar volêi, jogar videogame, falhar em aprender violão entre outros. E minha experiência previa com programação é quase nula e espero me ferrar nessa matéria." #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
